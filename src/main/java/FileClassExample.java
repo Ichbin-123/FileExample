09f41e986f9c209f493bc5084351c2b0830cbf7d
Marco Fuchs <furfaro.markus@gmail.com>
2025-09-03 21:17:18 +0200

    Primo Commit

--- src/main/java/FileClassExample.java
+++ src/main/java/FileClassExample.java
@@ -1,15 +1,16 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.Date;
 
 public class FileClassExample {
     public static void main(String[] args) {
         // Creaimo un oggetto
         File mioFile = new File("mio_file.txt");
 
-        if(mioFile !=null)
+        if(mioFile !=null) // non controllo così l'esistenza del file.....
             System.out.println("mio_file.txt");
 
-        // Controllare se il file esiste
+        // Controllare se il file esiste. Co^si controllo l'esistenza del file
         System.out.println(
                 "Il file esiste? " + mioFile.exists()
         );
@@ -28,6 +29,22 @@ public class FileClassExample {
         System.out.println(
                 "Il file esiste? " + mioFile.exists()
         );
+
+        // Info sui file
+        if(mioFile.exists()){
+            System.out.println("\n Nome: " + mioFile.getName());
+            System.out.println("Percorso: " + mioFile.getAbsolutePath());
+            System.out.println("Directory?: " + mioFile.isDirectory());
+            System.out.println("File?: " + mioFile.isFile());
+            System.out.println("Dimensioni: " + mioFile.length());
+            System.out.println("Ultima modifica: " + new Date(mioFile.lastModified()));
+        }
+
+        // Crea una Directory
+        File miaDirectory = new File("mia_directory");
+        boolean created = miaDirectory.mkdir();
+        if(created) System.out.println("Directory " + miaDirectory.getName() + " creata con successo");
+        else System.out.println("Directory " + miaDirectory.getName() + " gia presente");
 
     }
 }
